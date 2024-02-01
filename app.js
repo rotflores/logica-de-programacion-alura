bc1f88baede37c98c3b2afe0eca62c8d73b6ee14
rotflores <rotflores@gmail.com>
2024-02-01 12:16:42 -0600

    agrego funciones en html y js

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
 let titulo = document.querySelector("h1");
 titulo.innerHTML = "juego del numero secreto";
+let parrafo = document.querySelector("p");
+parrafo.innerHTML = "Escoge un numero del 1 - 100";
 
+function intentarAdivinar {
+    
+}
